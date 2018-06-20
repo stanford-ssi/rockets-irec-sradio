417d9c44b9c82419cb04b7673f9bd89d0f1d1e3f
Tim Vrakas <tim.vrakas@gmail.com>
2018-06-20 00:12:58 -0700

    We won't die now

--- src/main/beacon.cpp
+++ src/main/beacon.cpp
@@ -21,18 +21,20 @@
 
 #include <Arduino.h>
 #include "SRADio.h"
+#include "irec-rf.h"
 
 uint8_t tx_message_length = MAX_MSG_LENGTH;
 uint8_t tx_message[MAX_MSG_LENGTH];
 uint8_t rx_message[MAX_MSG_LENGTH];
+rf_up_t upPacket;
 
 SRADio SRADio1;
 
 int main()
 {
     Serial.begin(115200);
-    pinMode(4,OUTPUT);
-    digitalWrite(4,HIGH);
+    pinMode(4, OUTPUT);
+    digitalWrite(4, HIGH);
 
     for (int i = 0; i < 10; i++)
     {
@@ -43,12 +45,17 @@ int main()
 
     SRADio1.configureRF();
 
-    for (int i = 0; i < MAX_MSG_LENGTH; i++)
-    {
-        tx_message[i] = 255;
-        Serial.print(" " + tx_message[i]);
-    }
-    Serial.println();
+    upPacket.arm_payload = 0;
+    upPacket.charge1 = 0;
+    upPacket.charge2 = 0;
+    upPacket.charge3 = 0;
+    upPacket.charge4 = 0;
+    upPacket.charge1_parity = 0;
+    upPacket.charge2_parity = 0;
+    upPacket.charge3_parity = 0;
+    upPacket.charge4_parity = 0;
+
+    memcpy(&tx_message, &upPacket, sizeof(rf_up_t));
 
     for (int i = 10; i < 20; i++)
     {
@@ -56,9 +63,9 @@ int main()
         Serial.printf("delay%i\r\n", i);
     }
 
-    uint32_t pktTimer = millis(); //timer between tx packets
-    uint32_t listenTimer = millis();         //time of last rx'd packet
-    uint32_t sendTimer = millis();             //time we started beaconing
+    uint32_t pktTimer = millis();    //timer between tx packets
+    uint32_t listenTimer = millis(); //time of last rx'd packet
+    uint32_t sendTimer = millis();   //time we started beaconing
     bool sending = false;
     int i = 0;
 
@@ -75,7 +82,8 @@ int main()
                 pktTimer = millis();
                 Serial.println("sent");
             }
-            if(millis() > sendTimer + 5000){
+            if (millis() > sendTimer + 5000)
+            {
                 Serial.println("Taking a break...");
                 sending = false;
                 listenTimer = millis();
@@ -89,13 +97,14 @@ int main()
                 Serial.println("Heard Packet");
                 listenTimer = millis();
             }
-            if(millis() > listenTimer + 5000){
+            if (millis() > listenTimer + 5000)
+            {
                 Serial.println("Scilence... Starting Beacon!");
                 sending = true;
                 sendTimer = millis();
             }
         }
-        digitalWrite(4,sending);
+        digitalWrite(4, sending);
         Serial.println("loop");
         delay(100);
     }
